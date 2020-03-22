f0a66ed917195445817530af90425d7472cbdab3
Dimo Angelov <dangelov@uwo.ca>
2020-03-22 17:48:37 -0400

    updating setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="top2vec",
+    version="1.0.0",
+    author="Dimo Angelov",
+    author_email="dimo.angelov@gmail.com",
+    description="Topic2Vector learns jointly embedded topic, document and word vectors.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ddangelov/Top2Vec",
+    packages=setuptools.find_packages(),
+    keywords="topic modeling",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'gensim',
+        'umap-learn',
+        'hdbscan',
+        'sklearn'
+      ],
+    python_requires='>=3.6',
+)
