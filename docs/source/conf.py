c4794579c61917376892edad943a28f6ccd62158
Dimo Angelov <dangelov@uwo.ca>
2020-03-25 09:34:57 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+import sphinx_rtd_theme
+from recommonmark.parser import CommonMarkParser
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -48,15 +50,13 @@ exclude_patterns = []
 #
 #html_theme = 'alabaster'
 
-import sphinx_rtd_theme
+
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-from recommonmark.parser import CommonMarkParser
 
 master_doc = 'index'
 
