76513f488df27caf7c1befd463dcf4ebab844018
Dimo Angelov <dimo_angelov@hotmail.com>
2021-07-09 14:21:36 -0400

    updating version

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = '2020, Dimo Angelov'
 author = 'Dimo Angelov'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.25'
+release = '1.0.26'
 
 
 # -- General configuration ---------------------------------------------------
