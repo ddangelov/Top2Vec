0afe4d4251c0386b371023654fda1acae7c28be7
ShawnCodesABit <ShawnCodesOS@gmail.com>
2022-02-25 11:19:35 +1300

    Base functions for elbow finding and different distance metrics.

---
+++ top2vec/elbow_finding.py
@@ -0,0 +1,146 @@
+"""Allow finding an elbow for cut-off heuristics.
+
+Will eventually be rolled directly into Top2Vec.
+
+Author: Shawn
+License: BSD 3 clause
+"""
+from typing import Optional
+import numpy as np
+from numpy.typing import NDArray, ArrayLike
+
+# Prevent typos
+__EUCLIDEAN_STR = "euclidean"
+__MANHATTAN_STR = "manhattan"
+__UNIFORM_STR = "uniform"
+
+
+def find_elbow_index(
+    values: ArrayLike, metric: Optional[str] = __EUCLIDEAN_STR
+) -> Optional[int]:
+    """Finds the "elbow" in a series of descending real values.
+
+    Example uses include selecting the number of topics and determining
+    when there is a jump in a distance metric.
+
+    Parameters
+    ----------
+    values: ArrayLike
+        A 1d array (or list) of real values.
+
+    Returns
+    -------
+    int, optional
+        The index of the farthest point from the comparison slope
+        when all provided data has been sorted in descending order.
+        Will return None if provided a None value or empty list.
+    metric: Optional[str]
+        Which distance metric to use when comparing with the line.
+        One of ("euclidean", "manhattan", "uniform").
+
+    Notes
+    -----
+    The comparison slope is created by assuming a linear descent
+    from the largest to the smallest provided value. The elbow
+    can then be thought of as a value which diverges the most
+    and can then be used as a cut-off value.
+    """
+    if values is None:
+        return None
+    # Make sure the provided values are a sorted numpy array
+    sorted_values = -np.sort(-np.array(values))
+    if sorted_values.size == 0:
+        return None
+    if sorted_values.size == 1:
+        return 0
+    if len(sorted_values.shape) != 1:
+        raise ValueError("Elbow finding must be a 1-D Array")
+    slope = (sorted_values[-1] - sorted_values[0]) / (sorted_values.size - 1)
+    y_intercept = sorted_values[0]
+
+    distances = get_distances_from_line(
+        sorted_values, slope, y_intercept, metric=metric
+    )
+    return distances.argmax()
+
+
+def __euclidean_distance(x1: float, y1: float, x2: float, y2: float) -> float:
+    """Calculate the L2 distance between two points."""
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+
+def __manhattan_distance(x1: float, y1: float, x2: float, y2: float) -> float:
+    """Calculate the L1 distance between two points"""
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def __uniform_distance(x1: float, y1: float, x2: float, y2: float) -> float:
+    """Calculate the L-infinity distance between two points"""
+    return max(abs(x1 - x2), abs(y1 - y2))
+
+
+def get_distances_from_line(
+    values: ArrayLike,
+    comparison_slope: float,
+    comparison_y_intercept: float,
+    metric: Optional[str] = __EUCLIDEAN_STR,
+) -> NDArray[np.float64]:
+    """Finds the shortest distance for all provided values from a provided line.
+
+    Handles if the shortest distance to the line actually happens between
+    two regularly listed points with integer X values.
+
+    Parameters
+    ----------
+    values : ArrayLike
+        A 1d array (or list) of y values such that index 0 is y(0).
+    comparison_slope : float
+        The slope of the line to compare values with.
+    comparison_y_intercept : float
+        The y intercept of the line to compare values with.
+    metric: Optional[str]
+        Which distance metric to use when comparing with the line.
+        One of ("euclidean", "manhattan", "uniform").
+
+    Returns
+    -------
+    NDArray[np.float64]
+        A 1d array of the shortest euclidean distance for each
+        value to the provided line.
+    """
+    # This is all 2d, so euclidean seems like it should be the best by default
+    if metric == __EUCLIDEAN_STR:
+        dist_fun = __euclidean_distance
+    elif metric == __MANHATTAN_STR:
+        dist_fun = __manhattan_distance
+    elif metric == __UNIFORM_STR:
+        dist_fun = __uniform_distance
+    else:
+        raise ValueError(
+            f"Illegal metric - '{metric}'.\
+           Must be one of [{__EUCLIDEAN_STR}, {__MANHATTAN_STR}, {__UNIFORM_STR}]"
+        )
+    try:
+        distances = np.zeros(values.size)
+        to_examine = range(values.size)
+    except AttributeError:
+        # handed a list rather than a numpy array
+        distances = np.zeros(len(values))
+        to_examine = range(len(values))
+    perp_slope = comparison_slope * -1
+    # only compute this once
+    divisor = comparison_slope - perp_slope
+    for x in to_examine:
+        instance_y = values[x]
+        # special case: slope of 0
+        if divisor == 0:
+            distances[x] = abs(instance_y - comparison_y_intercept)
+        else:
+            # Which y value are we computing distance for
+            instance_y_intercept = instance_y - perp_slope * x
+
+            comparison_x = (instance_y_intercept - comparison_y_intercept) / divisor
+            comparison_y = comparison_slope * comparison_x + comparison_y_intercept
+
+            distances[x] = dist_fun(x, instance_y, comparison_x, comparison_y)
+    return np.array(distances)
---
+++ top2vec/tests/test_elbowfinding.py
@@ -0,0 +1,180 @@
+from itertools import permutations
+
+import pytest
+import numpy as np
+
+from top2vec.elbow_finding import (
+    find_elbow_index,
+    get_distances_from_line,
+    __euclidean_distance,
+    __manhattan_distance,
+    __uniform_distance,
+)
+
+
+# Make sure our metrics work the way we think
+def test_euclidean_distance():
+    assert __euclidean_distance(0, 0, 0, 0) == 0
+    assert __euclidean_distance(2, 2, 2.5, 2.5) == (2 * ((2.5 - 2.0) ** 2)) ** 0.5
+    assert __euclidean_distance(1, 1, 2.5, 2.5) == (2 * ((2.5 - 1.0) ** 2)) ** 0.5
+    assert __euclidean_distance(0, 0, 2.5, 2.5) == (2 * (2.5**2)) ** 0.5
+    assert __euclidean_distance(-2.5, -2.5, 2.5, 2.5) == (2 * (5**2)) ** 0.5
+    assert __euclidean_distance(-2.5, 0, 2.5, 2.5) == ((5**2) + (2.5**2)) ** 0.5
+    assert __euclidean_distance(0, -2.5, 2.5, 2.5) == ((5**2) + (2.5**2)) ** 0.5
+    assert __euclidean_distance(2.5, 2.5, 2, 2) == (2 * ((2.5 - 2.0) ** 2)) ** 0.5
+    assert __euclidean_distance(2.5, 2.5, 1, 1) == (2 * ((2.5 - 1.0) ** 2)) ** 0.5
+    assert __euclidean_distance(2.5, 2.5, 0, 0) == (2 * (2.5**2)) ** 0.5
+    assert __euclidean_distance(2.5, 2.5, -2.5, -2.5) == (2 * (5**2)) ** 0.5
+    assert __euclidean_distance(2.5, 2.5, -2.5, 0) == ((5**2) + (2.5**2)) ** 0.5
+    assert __euclidean_distance(2.5, 2.5, 0, -2.5) == ((5**2) + (2.5**2)) ** 0.5
+
+
+def test_manhattan_distance():
+    assert __manhattan_distance(0, 0, 0, 0) == 0
+    assert __manhattan_distance(2, 2, 2.5, 2.5) == 1
+    assert __manhattan_distance(1, 1, 2.5, 2.5) == 3
+    assert __manhattan_distance(0, 0, 2.5, 2.5) == 5
+    assert __manhattan_distance(-2.5, -2.5, 2.5, 2.5) == 10
+    assert __manhattan_distance(-2.5, 0, 2.5, 2.5) == 7.5
+    assert __manhattan_distance(0, -2.5, 2.5, 2.5) == 7.5
+    assert __manhattan_distance(2.5, 2.5, 2, 2) == 1
+    assert __manhattan_distance(2.5, 2.5, 1, 1) == 3
+    assert __manhattan_distance(2.5, 2.5, 0, 0) == 5
+    assert __manhattan_distance(2.5, 2.5, -2.5, -2.5) == 10
+    assert __manhattan_distance(2.5, 2.5, -2.5, 0) == 7.5
+    assert __manhattan_distance(2.5, 2.5, 0, -2.5) == 7.5
+
+
+def test_uniform_distance():
+    assert __uniform_distance(0, 0, 0, 0) == 0
+    assert __uniform_distance(2, 2, 2.5, 2.5) == 0.5
+    assert __uniform_distance(1, 1, 2.5, 2.5) == 1.5
+    assert __uniform_distance(0, 0, 2.5, 2.5) == 2.5
+    assert __uniform_distance(-2.5, -2.5, 2.5, 2.5) == 5
+    assert __uniform_distance(-2.5, 0, 2.5, 2.5) == 5
+    assert __uniform_distance(0, -2.5, 2.5, 2.5) == 5
+    assert __uniform_distance(2.5, 2.5, 2, 2) == 0.5
+    assert __uniform_distance(2.5, 2.5, 1, 1) == 1.5
+    assert __uniform_distance(2.5, 2.5, 0, 0) == 2.5
+    assert __uniform_distance(2.5, 2.5, -2.5, -2.5) == 5
+    assert __uniform_distance(2.5, 2.5, -2.5, 0) == 5
+    assert __uniform_distance(2.5, 2.5, 0, -2.5) == 5
+
+
+def compare_numpy_arrays(array_a, array_b):
+    if array_a is None and array_b is None:
+        return True
+    elif array_a is None or array_b is None:
+        return False
+
+    if array_a.size == 0 and array_b.size == 0:
+        return True
+    elif array_a.size == 0 or array_b.size == 0:
+        return False
+
+    if array_a.shape != array_b.shape:
+        return False
+    return (array_a == array_b).all()
+
+
+def distances_from_line_helper(metric, expected_distance):
+    # If we give it an empty list then things should
+    # be an empty array
+    assert compare_numpy_arrays(get_distances_from_line([], 1, 0), np.array([]))
+
+    y_int = 5
+    slope = -1
+
+    # Items which follow the slope exactly should be all 0 for all metrics
+    assert compare_numpy_arrays(
+        get_distances_from_line([5, 4, 3, 2, 1], slope, y_int, metric), np.zeros(5)
+    )
+    # Order DOES matter
+    assert not compare_numpy_arrays(
+        get_distances_from_line([3, 4, 5, 2, 1], slope, y_int, metric), np.zeros(5)
+    )
+    assert compare_numpy_arrays(
+        get_distances_from_line([5, 4, 2, 2, 1], slope, y_int, metric),
+        np.array([0, 0, expected_distance, 0, 0]),
+    )
+    # Should be symmetrical
+    assert compare_numpy_arrays(
+        get_distances_from_line([5, 4, 2, 3, 1], slope, y_int, metric),
+        np.array([0, 0, expected_distance, expected_distance, 0]),
+    )
+
+    # What if we did a numpy array rather than just a list of numbers?
+    assert compare_numpy_arrays(
+        get_distances_from_line(np.array([5, 4, 3, 2, 1]), slope, y_int, metric),
+        np.zeros(5),
+    )
+    assert not compare_numpy_arrays(
+        get_distances_from_line(np.array([3, 4, 5, 2, 1]), slope, y_int, metric),
+        np.zeros(5),
+    )
+    assert compare_numpy_arrays(
+        get_distances_from_line(np.array([5, 4, 2, 2, 1]), slope, y_int, metric),
+        np.array([0, 0, expected_distance, 0, 0]),
+    )
+    assert compare_numpy_arrays(
+        get_distances_from_line(np.array([5, 4, 2, 3, 1]), slope, y_int, metric),
+        np.array([0, 0, expected_distance, expected_distance, 0]),
+    )
+
+    # What about the slope of zero? Should be the same for all metrics.
+    assert compare_numpy_arrays(
+        get_distances_from_line([1, 2, 3], 0, 1, metric), np.array([0, 1, 2])
+    )
+    assert compare_numpy_arrays(
+        get_distances_from_line(np.array([5, 4, 2, 3, 1]), 0, 0, metric),
+        np.array([5, 4, 2, 3, 1]),
+    )
+
+
+def test_get_distances_from_line_euclidean():
+    # this distance should be between (2,2) and (2.5, 2.5)
+    distances_from_line_helper("euclidean", (2 * ((2.5 - 2.0) ** 2)) ** 0.5)
+
+
+def test_get_distances_from_line_manhattan():
+    # this distance should be between (2,2) and (2.5, 2.5)
+    distances_from_line_helper("manhattan", 1)
+
+
+def test_get_distances_from_line_uniform():
+    # this distance should be between (2,2) and (2.5, 2.5)
+    distances_from_line_helper("uniform", 0.5)
+
+
+def test_find_elbow_index():
+    # Tell me what the farthest shortest distance from the line is!
+    assert find_elbow_index([]) is None
+    assert find_elbow_index(None) is None
+    with pytest.raises(ValueError):
+        find_elbow_index([[1, 2, 22]])
+
+    def elbow_index_helper(base_data, expected):
+        # Make sure that the order doesn't matter here and neither
+        # does NP array or list
+        perms = permutations(base_data)
+        # In 2d spaces the end result of the different norms should be identical
+        for metric in ["euclidean", "manhattan", "uniform"]:
+            for instance in perms:
+                assert find_elbow_index(instance, metric) == expected
+                assert find_elbow_index(np.array(instance), metric) == expected
+
+    # Anything of size 1 will return the first
+    elbow_index_helper([2], 0)
+    elbow_index_helper([0.022], 0)
+
+    # If everything is equal we should get the first value
+    elbow_index_helper([5, 4, 3, 2, 1], 0)
+
+    # Now a real value
+    elbow_index_helper([5, 4, 2, 2, 1], 2)
+
+    # What if we have equidistent points?
+    elbow_index_helper([5, 4, 2, 1, 1], 2)
+
+    # What if we have equidistent points?
+    elbow_index_helper([5, 4, 2, 1, 1, 0], 2)
